336e4af3384900f0af76a0f98dbe3f90a32cdf82
Oussama <Messamessus@yahoo.com>
2025-03-31 15:21:35 +0000

    update index project path

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
   : [
     "http://localhost:5173",
-      "https://wahret-zmen-app-frontend-six.vercel.app",
+      "https://wahret-zmen-app-frontend-seven.vercel.app",
       
     ];
 
